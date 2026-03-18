081acd5fddaf93c9b409abdcae353467f7376109
bettytadege <bettytadege13@gmail.com>
2026-03-18 18:33:21 +0300

    feat:create dashboard layout with sidebar and header

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import DashboardHeader from "@/app/components/common/DashboardHeader"
+
+import { useAppSelector } from "@/app/hooks/hooks"
+import { Role } from "../types/types"
+import Sidebar from "../components/sidebar/SideBar"
+import { useRouter } from "next/navigation"
+
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+ const router = useRouter()
+  const role = useAppSelector((state) => state.auth.user?.role) as Role
+  console.log('role 🎉🎉🎉',role)
+  if (!role) return router.push('/')
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+
+      <Sidebar role={role} />
+
+      <div className="flex flex-1 flex-col">
+
+        <div className="sticky top-0 z-50">
+          <DashboardHeader />
+        </div>
+
+        <main className="flex-1 overflow-y-auto p-6 bg-[#FAFAFA]">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
+  )
+}
