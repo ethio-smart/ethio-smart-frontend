e503f288edd6853669eb49ab78a50b039a8178a3
bettytadege <bettytadege13@gmail.com>
2026-03-18 18:32:20 +0300

    chore: remove commented lines

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,10 @@
-// import AboutUs from "./components/AboutUs";
-// import BecomeTaskerSection from "./components/BecomeTaskerSection";
-// import FAQ from "./components/FAQ";
-// import Hero from "./components/Hero";
-// import HowItWorks from "./components/HowItworks";
-// import ServiceCategories from "./components/ServiceCategories";
-// import StatsBar from "./components/StatsBar";
-// import Testimonial from "./components/Testimonial";
+
 
 import MainLayout from "./(main)/layout"
 import Home from "./(main)/page"
 
 
 
-// export default function Home() {
-//   return (
-//     <div className="text-xl w-full min-h-screen bg-white">
-    
-//          <Hero/> 
-//          <StatsBar/>
-//          <div className="h-px mt-5 bg-neutral-200"></div>
-//          {/* <hr  className="pt-10"/> */}
-//          <HowItWorks/>
-//          <ServiceCategories/>
-//          <AboutUs/>
-//          <Testimonial/>
-//          <BecomeTaskerSection/>
-//          <FAQ/>
-      
-//     </div>
-//   );
-// }
 
 
 function page() {
