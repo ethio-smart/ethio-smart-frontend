26cd6448f1444de37eb27d0568dc7087b6e25222
bettytadege <bettytadege13@gmail.com>
2026-03-18 17:28:19 +0300

    feat:implement auth slice with async thunks for authentication

---
+++ app/store/slices/authSlice.ts
@@ -0,0 +1,84 @@
+
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit"
+
+import { User } from "@/app/types/types"
+import { api } from "@/app/utils/axiosinstance"
+
+// Async thunk to fetch the current logged-in user
+export const fetchUser = createAsyncThunk<User>(
+  "auth/fetchUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem("accessToken") 
+      if (!token) throw new Error("No token found")
+      
+      const response = await api.get("/users/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      return response.data 
+    } catch (err: any) {
+      return rejectWithValue(err.response?.data || err.message || "Failed to fetch user")
+    }
+  }
+)
+
+interface AuthState {
+  user: User | null
+  email: string | null
+  loading: boolean
+  error: string | null
+}
+
+const initialState: AuthState = {
+  user: null,
+  email: null,
+  loading: false,
+  error: null
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload
+    },
+    setEmail: (state, action: PayloadAction<string>) => {
+      state.email = action.payload
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload
+    },
+    setError: (state, action: PayloadAction<string | null>) => {
+      state.error = action.payload
+    },
+    logout: (state) => {
+      state.user = null
+      state.email = null
+      localStorage.removeItem("accessToken")
+      state.loading = false
+      state.error = null
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUser.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(fetchUser.fulfilled, (state, action: PayloadAction<User>) => {
+        state.user = action.payload
+        state.loading = false
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload as string
+        state.user = null
+      })
+  }
+})
+
+export const { setUser, setEmail, setLoading, setError, logout } = authSlice.actions
+export default authSlice.reducer
