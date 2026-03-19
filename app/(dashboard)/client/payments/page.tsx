b59fb49fbdf7db48cd2450948758542b274ff8a0
bettytadege <bettytadege13@gmail.com>
2026-03-19 10:52:09 +0300

    feat(client-dashboard): add payment page

---
+++ app/(dashboard)/client/payments/page.tsx
@@ -0,0 +1,27 @@
+import PaymentHistory from "@/app/components/dashboard/client/payment/PaymentHistory";
+import PaymentStats from "@/app/components/dashboard/client/payment/PaymentStats";
+import SpendingChart from "@/app/components/dashboard/client/payment/SpendingChart";
+
+
+export default function PaymentsPage() {
+  return (
+    <div className="space-y-6 p-6">
+
+      <div>
+        <h1 className="text-2xl font-bold">
+          Payment History & Spending Trends
+        </h1>
+        <p className="text-muted-foreground text-sm">
+          Monitor your service expenditures and manage escrow payments securely.
+        </p>
+      </div>
+
+      <PaymentStats/>
+
+      <SpendingChart />
+
+      <PaymentHistory />
+
+    </div>
+  )
+}
