dcb636beeac7b35b7b15c71f0c093c2106354be3
bettytadege <bettytadege13@gmail.com>
2026-03-24 14:46:04 +0300

    refactor: move user fetch to global dispatch

--- app/AppInitializer.tsx
+++ app/AppInitializer.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './hooks/hooks';
 import { fetchCategories } from './store/slices/categorySlice';
+import { fetchUser } from './store/slices/authSlice';
 
 
 export default function AppInitializer() {
@@ -13,6 +14,7 @@ export default function AppInitializer() {
     if (!categories.length) {
       dispatch(fetchCategories());
     }
+        dispatch(fetchUser())
   }, [dispatch, categories.length]);
 
   return null;
