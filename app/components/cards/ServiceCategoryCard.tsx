a5e0b40435543f17a167816d8117554697692727
bettytadege <bettytadege13@gmail.com>
2026-03-26 21:44:09 +0300

    feat: add dynamic category links to task creation page

--- app/components/cards/ServiceCategoryCard.tsx
+++ app/components/cards/ServiceCategoryCard.tsx
@@ -1,5 +1,6 @@
 import { LucideIcon } from "lucide-react"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type ServiceCategoryCardProps={
   icon:LucideIcon,
@@ -8,13 +9,16 @@ type ServiceCategoryCardProps={
 }
 
 function ServiceCategoryCard({icon,bgColor,name}:ServiceCategoryCardProps) {
+  // const { category } = useParams()
+  
   return (
     <div className="bg-white h-40 rounded-lg shadow flex justify-center items-center">
       <div className="flex flex-col items-center gap-2">
       <div className={`bg-secondary flex items-center justify-center size-16 rounded-md text-primary`} style={{background:bgColor}}>
         {icon}
+        {/* <Icon/> */}
       </div>
-        <Link href={'/tasks/create'} className="font-semibold hover:underline">{name}</Link >
+        <Link href={`/tasks/create/${name.toLowerCase()}`} className="font-semibold hover:underline">{name}</Link >
         </div>
     </div>
   )
