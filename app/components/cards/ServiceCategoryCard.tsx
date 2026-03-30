b4077ea660fea41ea33c0060740d0102045a48da
bettytadege <bettytadege13@gmail.com>
2026-03-30 04:56:51 +0300

    fix: correct navigation link for service request routes

--- app/components/cards/ServiceCategoryCard.tsx
+++ app/components/cards/ServiceCategoryCard.tsx
@@ -1,6 +1,7 @@
+
 import { LucideIcon } from "lucide-react"
 import Link from "next/link"
-import { useParams } from "next/navigation"
+
 
 type ServiceCategoryCardProps={
   icon:LucideIcon,
@@ -9,17 +10,21 @@ type ServiceCategoryCardProps={
   categoryId:string
 }
 
-function ServiceCategoryCard({icon,bgColor,name}:ServiceCategoryCardProps) {
+function ServiceCategoryCard({icon,bgColor,name,categoryId}:ServiceCategoryCardProps) {
   // const { category } = useParams()
+  // const{selectedCategory}=useAppSelector(state=>state.category)
+  // const dispatch=useAppDispatch()
+  console.log('selected category in card',categoryId)
+
   
   return (
     <div className="bg-white h-40 rounded-lg shadow flex justify-center items-center">
       <div className="flex flex-col items-center gap-2">
-      <div className={`bg-secondary flex items-center justify-center size-16 rounded-md text-primary`} style={{background:bgColor}}>
+      <div  className={`bg-secondary flex items-center justify-center size-16 rounded-md text-primary`} style={{background:bgColor}}>
         {icon}
         {/* <Icon/> */}
       </div>
-        <Link href={`/tasks/create/${name.toLowerCase()}`} className="font-semibold hover:underline">{name}</Link >
+        <Link   href={`/request/create/${categoryId}?name=${name.toLowerCase()}`} className="font-semibold hover:underline">{name}</Link >
         </div>
     </div>
   )
