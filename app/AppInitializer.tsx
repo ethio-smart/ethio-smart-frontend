720bf495410ed1efd7d93b4768ab35fa72f78bf9
bettytadege <bettytadege13@gmail.com>
2026-03-30 11:10:59 +0300

    fix: add token dependency to app initializer

--- app/AppInitializer.tsx
+++ app/AppInitializer.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useEffect } from 'react';
@@ -5,17 +6,28 @@ import { useAppDispatch, useAppSelector } from './hooks/hooks';
 import { fetchCategories } from './store/slices/categorySlice';
 import { fetchUser } from './store/slices/authSlice';
 
-
 export default function AppInitializer() {
+  console.log("AppInitializer mounted");
+
   const dispatch = useAppDispatch();
   const { categories } = useAppSelector((state) => state.category);
 
   useEffect(() => {
-    if (!categories.length) {
+    
+    if (!categories?.length) {
       dispatch(fetchCategories());
     }
-        dispatch(fetchUser())
-  }, [dispatch, categories.length]);
+  }, [dispatch, categories?.length]);   
+
+  useEffect(() => {
+    const token = localStorage.getItem('access-token');
+    
+    if (token) {
+      dispatch(fetchUser());
+    }
+  }, [dispatch]);   
 
   return null;
 }
+
+
