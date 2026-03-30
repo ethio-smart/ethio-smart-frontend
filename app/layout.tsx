c5ad25a1db335acc71ce66c7842e9d81f6d7e541
bettytadege <bettytadege13@gmail.com>
2026-03-30 11:11:26 +0300

    chore:remove app initializer from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} ${geistMono.variable} antialiased`}>
         
         <ReduxProvider>
-          <AppInitializer />   
+          {/* <AppInitializer />    */}
           {children}
         </ReduxProvider>
 
