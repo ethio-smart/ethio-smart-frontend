e2408284c20c8fae2f1ca58146180ca2f5851f17
bettytadege <bettytadege13@gmail.com>
2026-03-31 21:26:16 +0300

    refactor: rename worker to tasker across application

--- app/components/common/DashboardHeader.tsx
+++ app/components/common/DashboardHeader.tsx
@@ -28,7 +28,7 @@ export default function DashboardHeader() {
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
 
         <Input
-          placeholder="Search workers or services..."
+          placeholder="Search taskers or services..."
           className="pl-9"
         />
       </div>
