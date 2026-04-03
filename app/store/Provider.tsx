eab6dfc56ab5f1b960b3343eabd64b9c75f20b98
bettytadege <bettytadege13@gmail.com>
2026-04-03 17:44:33 +0300

    refactor: move initializer from layout to Redux provider

--- app/store/Provider.tsx
+++ app/store/Provider.tsx
@@ -3,6 +3,7 @@
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
 import { store, persistor } from "./store"
+import AppInitializer from "../AppInitializer"
 
 export default function ReduxProvider({
   children,
@@ -11,6 +12,7 @@ export default function ReduxProvider({
 }) {
   return (
     <Provider store={store}>
+      <AppInitializer/>
       <PersistGate loading={null} persistor={persistor}>
         {children}
       </PersistGate>
