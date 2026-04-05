fb55b6144c3c0cc38e8ad2fc73da67aee00fa1c3
bettytadege <bettytadege13@gmail.com>
2026-04-05 22:09:55 +0300

    feat: configure token handling in axios instance

--- app/utils/axiosinstance.ts
+++ app/utils/axiosinstance.ts
@@ -1,8 +1,18 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://ethio-smart-backend.onrender.com",
+  baseURL: "https://ethio-smart-backend-xttm.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
+})
+//
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken")
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
