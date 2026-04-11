22ce8cb8c5156ab5598b63fd456d896075fa2209
bettytadege <bettytadege13@gmail.com>
2026-04-11 10:55:30 +0300

    feat:add footer in main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/landing/Header"
+import Footer from "../components/layout/Footer"
 
 export default function MainLayout({
   children,
@@ -11,6 +12,7 @@ export default function MainLayout({
         <Header/>
       </div>
       {children}
+      <Footer/>
     </>
   )
 }
