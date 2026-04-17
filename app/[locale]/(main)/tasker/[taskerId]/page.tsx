bc59499727fb7490f152b59276ed5d250ceeb534
bettytadege <bettytadege13@gmail.com>
2026-04-17 10:16:25 +0300

    feat: add tasker profile page

---
+++ app/[locale]/(main)/tasker/[taskerId]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Reviews from "@/app/[locale]/components/tasker/Reviews"
+import SidebarCard from "@/app/[locale]/components/tasker/SidebarCard"
+import TaskerAbout from "@/app/[locale]/components/tasker/TaskerBio"
+import TaskerProfileHeader from "@/app/[locale]/components/tasker/TaskerProfileHeader"
+import TaskerSkills from "@/app/[locale]/components/tasker/TaskerSkills"
+
+
+import { useSearchParams } from "next/navigation"
+
+
+export default function TaskerProfilePage() {
+  // const tasker = dummyTasker
+  const searchParams = useSearchParams()
+
+  const taskerParam = searchParams.get("tasker")
+
+  const tasker = taskerParam ? JSON.parse(taskerParam) : null
+  console.log('tasker profile',tasker)
+
+  return (
+    <>
+    <div className="max-w-6xl mx-auto py-8  bg-white ">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2 space-y-10 shadow p-10 rounded-2xl">
+          <TaskerProfileHeader tasker={tasker} />
+          <TaskerAbout bio={tasker.bio} />
+           <TaskerSkills services={tasker.services} />
+           
+          {/* <TaskerAvailability tasker={tasker} /> */}
+          <Reviews reviews={tasker.reviews} />
+        </div>
+        <SidebarCard tasker={tasker} />
+      </div>
+      </div>
+      </>
+   
+  )
+}
