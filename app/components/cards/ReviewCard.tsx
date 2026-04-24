c9c88d9e6b94dfb8c1fd326acaf99a9bff126043
bettytadege <bettytadege13@gmail.com>
2026-04-24 18:36:36 +0300

    feat: add ReviewCard component for displaying user reviews

---
+++ app/components/cards/ReviewCard.tsx
@@ -0,0 +1,46 @@
+
+
+import { Card, CardContent } from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Star } from "lucide-react"
+
+export const ReviewCard = ({ review }) => {
+  return (
+    <Card className="shadow-sm">
+      <CardContent className="p- flex gap-4">
+        
+        <Avatar size="lg">
+          <AvatarImage src={review.avatarUrl} alt={review.userName} />
+          <AvatarFallback  className={review.avatarColor}>
+            {review.initials}
+          </AvatarFallback>
+        </Avatar>
+
+        <div className="flex-1">
+          <div className="flex justify-between">
+            
+            <div>
+              <p className="font-semibold">{review.userName}</p>
+              <p className="text-sm flex items-center gap-1">
+                {review.rating}
+                <Star className="inline-block w-4 h-4 fill-yellow-400 text-yellow-400" />
+              </p>
+            </div>
+
+            <div className="text-xs text-muted-foreground text-right">
+              <p>{review.date}</p>
+              <Badge variant="secondary">{review.service}</Badge>
+            </div>
+
+          </div>
+
+          <p className="text-sm text-muted-foreground mt-2">
+            {review.comment}
+          </p>
+        </div>
+
+      </CardContent>
+    </Card>
+  )
+}
