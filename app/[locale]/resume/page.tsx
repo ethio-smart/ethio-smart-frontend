f67e04e59ccd59f75363be35d6a8981245cebc8e
bettytadege <bettytadege13@gmail.com>
2026-04-25 22:37:12 +0300

    feat: implement dynamic data handling in ResumePage component

--- app/[locale]/resume/page.tsx
+++ app/[locale]/resume/page.tsx
@@ -7,31 +7,57 @@ import Experience from "@/app/components/resume/Experience"
 import Certificate from "@/app/components/resume/Certificate"
 import Language from "@/app/components/resume/Language"
 import Profile from "@/app/components/resume/Profile"
+import { useSearchParams } from "next/navigation"
+import { useAppDispatch, useAppSelector } from "@/app/hooks/hooks"
+import { fetchResume } from "@/app/store/slices/resumeSlice"
+import { useEffect } from "react"
 
 
 export default function ResumePage() {
-  // const t = useTranslations('resume')
+  const searchParams = useSearchParams()
+  const taskerId = searchParams.get("taskerId")
+
+  const dispatch = useAppDispatch()
+  const { data, loading, error } = useAppSelector(
+    (state) => state.resume
+  )
+
+  useEffect(() => {
+    if (taskerId) {
+      dispatch(fetchResume(taskerId))
+    }
+  }, [taskerId, dispatch])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+  if (!data) return <p>No data</p>
+  console.log("taskerId:", taskerId)
 
   return (
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
-        
-         <Profile/>
+
+        <Profile
+          name={data.Name}
+          phone={data.Phone}
+          location={data.Location}
+          rating={data.Rating}
+        />
         {/*  Summary */}
-        <Summary/>
-      
+        <Summary summary={data.Summary} />
+
         {/* Skills */}
-    
-       <Services/>
+
+        <Services services={data.Services} />
         {/* Experience */}
-        <Experience/>
-       
+        <Experience experience={data.Experience} />
+
 
         {/* certificate */}
-        <Certificate/>
-        <Language/>
-        
+        <Certificate certifications={data.Certifications} />
+        <Language languages={data.Languages} />
+
       </div>
     </div>
   )
