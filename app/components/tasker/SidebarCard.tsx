330fcfda090b76979f6925ee3851c8b489043128
bettytadege <bettytadege13@gmail.com>
2026-04-25 22:36:34 +0300

    refactor: clean up SidebarCard component and implement dynamic locale for resume link

--- app/components/tasker/SidebarCard.tsx
+++ app/components/tasker/SidebarCard.tsx
@@ -1,8 +1,9 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Tasker } from "@/app/types/types"
-import { CheckCircle, FileText, SendHorizontal } from "lucide-react"
+import { CheckCircle, SendHorizontal } from "lucide-react"
 import Link from "next/link"
+import { useLocale } from "next-intl"
 
 
 export default function SidebarCard({
@@ -10,7 +11,7 @@ export default function SidebarCard({
 }: {
   tasker: Tasker
 }) {
- 
+  const locale = useLocale()
 
   return (
     <Card className="h-fit sticky top-10 shadow-none border-none md:border md:shadow" >
@@ -19,7 +20,7 @@ export default function SidebarCard({
         <div className="text-sm  space-y-1 hidden md:block">
           <p>Status: {tasker.status}</p>
           <div className="flex items-center gap-2">
-         {tasker.availability ? (
+            {tasker.availability ? (
               <CheckCircle className="text-green-500" size={16} />
             ) : (
               <CheckCircle className="text-gray-400" size={16} />
@@ -27,23 +28,30 @@ export default function SidebarCard({
             <span>
               {tasker.availability ? "Available for work" : "Currently unavailable"}
             </span>
-            </div>
+          </div>
         </div>
 
         <Button className="w-full py-5">
-          <SendHorizontal/>
+          <SendHorizontal />
           Send Request
         </Button>
 
-       
-         
-            <Link target="_blank" rel="noopener noreferrer" href={'/en/resume'} >
+
+
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href={{
+            pathname: `/${locale}/resume`,
+            query: { taskerId: tasker.id },
+          }}
+        >
           {/* <FileText color="black"/> */}
-                <Button variant="outline" className="w-full py-5">
-                    View Resume
-                </Button>
-                    </Link>
-      
+          <Button variant="outline" className="w-full py-5">
+            View Resume
+          </Button>
+        </Link>
+
         <p className="text-xs text-muted-foreground text-center leading-relaxed">
           You will only be charged after the tasker accepts your request.
         </p>
