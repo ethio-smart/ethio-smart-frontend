201a9051c7480fef5d679a325ec7f87542b20c1c
bettytadege <bettytadege13@gmail.com>
2026-07-05 11:20:37 +0300

    feat: update setUser reducer to accept null as a valid payload

--- app/store/slices/authSlice.ts
+++ app/store/slices/authSlice.ts
@@ -37,7 +37,7 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<User>) => {
+    setUser: (state, action: PayloadAction<User | null>) => {
       state.user = action.payload
     },
     setEmail: (state, action: PayloadAction<string>) => {
